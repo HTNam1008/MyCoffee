6e6f8bf01eb2ca1ad179f25b32da61d96377c938
21120185 <21120185@student.hcmus.edu.vn>
2023-12-19 14:17:17 +0700

    get

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -5,8 +5,12 @@ const { mutipleMongooseToObject } = require("../../util/mongoose");
 class HomeController{
     
     index(req,res,next){
-        const nTable=req.query.table;
-        console.log(nTable);
+      const tableId = req.query.table; // Lấy mã bàn từ param table trong URL
+      const table = tables[tableId]; // Lấy thông tin của bàn từ danh sách tables
+  
+      if (!table) {
+          res.status(404).send('Không tìm thấy thông tin của bàn');
+      } else {
         const user=req.session.user;
         var employee, admin;
         if (user){
@@ -27,6 +31,7 @@ class HomeController{
         })
         .catch(next); 
     }
+  }
 }
 
 const tables = {
@@ -34,27 +39,6 @@ const tables = {
   A2: { id: 'A2' },
   // Thông tin về các bàn khác...
 };
-
-class HomeController {
-  index(req, res, next) {
-    const tableId = req.query.table; // Lấy mã bàn từ param table trong URL
-    const table = tables[tableId]; // Lấy thông tin của bàn từ danh sách tables
-
-    if (!table) {
-        res.status(404).send('Không tìm thấy thông tin của bàn');
-    } else {
-      //const user = req.session.user;
-      Product.find({})
-        .then((products) => {
-          res.render("home", {
-            products: mutipleMongooseToObject(products),});
-            //user: user,
-          //});
-        })
-        .catch(next);
-    }
-  }
-}
 
 
 module.exports = new HomeController();
--- src/routes/home.js
+++ src/routes/home.js
@@ -3,10 +3,9 @@ const route = express.Router();
 
 const homeController = require("../app/controllers/HomeController");
 
-route.get('/', homeController.index);
 
 
-route.get('/?table=1',homeController.index);
+
 route.get('/',homeController.index);
 
 
