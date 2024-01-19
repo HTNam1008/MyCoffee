d858c918359502753abb168a4d01732f36a832ab
HTNam1008 <skyisthelimit.1008@gmail.com>
2024-01-19 10:31:47 +0700

    manage revenue

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -10,6 +10,8 @@ route.get('/showEmployees',isAuthenticated,adminController.indexEmployees);
 route.get('/showProducts',isAuthenticated,adminController.indexProducts);
 route.get('/homepage',isAuthenticated,adminController.homepage);
 route.get('/orderHistory',isAuthenticated,adminController.orderHistory);
+route.get('/reportRevenue',isAuthenticated, adminController.reportRevenue);
+route.post('/getRevenueDate', adminController.getRevenueDate);
 
 route.post('/getDate',adminController.getDate);
 module.exports=route;
